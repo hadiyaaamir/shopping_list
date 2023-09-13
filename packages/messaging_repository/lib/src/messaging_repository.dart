b78dfaaa5cb521bf7aef3cb9cf726654d842c0ca
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-09-13 16:22:35 +0500

    messaging repository

---
+++ packages/messaging_repository/lib/src/messaging_repository.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+import 'dart:io' show Platform;
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart' as http;
+
+part 'messaging_repository_firebase.dart';
+
+abstract class MessagingRepository {}
---
+++ packages/messaging_repository/lib/src/messaging_repository_firebase.dart
@@ -0,0 +1,135 @@
+part of 'messaging_repository.dart';
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('Title: ${message.notification?.title}');
+  print('Body: ${message.notification?.body}');
+  print('Payload: ${message.data}');
+}
+
+String fcmToken = '';
+
+class MessagingRepositoryFirebase extends MessagingRepository {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications',
+    importance: Importance.defaultImportance,
+  );
+
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
+    fcmToken = await _firebaseMessaging.getToken() ?? '';
+    print('Token: $fcmToken');
+
+    initPushNotifications();
+    initLocalNotifications();
+  }
+
+  Future initPushNotifications() async {
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if (notification == null) return;
+
+      _localNotifications.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidChannel.id,
+            _androidChannel.name,
+            channelDescription: _androidChannel.description,
+            icon: '@drawable/ic_launcher',
+          ),
+        ),
+        payload: jsonEncode(message.toMap()),
+      );
+    });
+  }
+
+  void handleMessage(RemoteMessage? message) {
+    if (message == null) return;
+    //TODO: navigate to correct screen
+    // navigatorKey.currentState?.push(Screen2.route(message: message));
+  }
+
+  Future initLocalNotifications() async {
+    const iOS = DarwinInitializationSettings();
+    const android = AndroidInitializationSettings('@drawable/ic_launcher');
+    const settings = InitializationSettings(android: android, iOS: iOS);
+
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (details) {
+        final message = RemoteMessage.fromMap(jsonDecode(details.payload!));
+        handleMessage(message);
+      },
+    );
+
+    if (Platform.isAndroid) {
+      final platform =
+          _localNotifications.resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin>();
+      await platform?.createNotificationChannel(_androidChannel);
+    } else if (Platform.isIOS) {
+      final platform =
+          _localNotifications.resolvePlatformSpecificImplementation<
+              IOSFlutterLocalNotificationsPlugin>();
+      await platform?.requestPermissions(alert: true, badge: true, sound: true);
+    }
+  }
+
+  void sendPushMessage({
+    required String token,
+    String title = '',
+    String body = '',
+  }) async {
+    const serverKey =
+        'AAAAVvI0CHs:APA91bFQhuWPbmD6cpNnTthSbGDrk0UufioEGrsPEHG22c_k3tezqobdVawSGKCqxoEw790zz0dgu4_nm1tNnCgJhCQEBc4rc-8FIg4NtuDLLcMLSt-1ISONQCjNVB2dirpyiJkulqtv';
+
+    if (token.isEmpty) {
+      print('token is empty');
+      return;
+    }
+
+    try {
+      await http.post(
+        Uri.parse('https://fcm.googleapis.com/fcm/send'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'key=$serverKey'
+        },
+        body: jsonEncode(<String, dynamic>{
+          'notification': <String, dynamic>{
+            'title': title,
+            'body': body,
+          },
+          'data': <String, dynamic>{
+            'status': 'done',
+            'title': title,
+            'body': body,
+          },
+          'to': token,
+        }),
+      );
+
+      print('notification sent to $token \n title: $title \n body: $body');
+    } catch (e) {
+      print("error push notification $e");
+    }
+  }
+}
